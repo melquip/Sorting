2c4f8f6a34dd30f7fe8b8868d9ccc308d20fbd6b
Melquisedeque Pereira <melqui1@live.com.pt>
2020-02-18 14:10:18 +0000

    Stretch: Completed merge_sort_in_place

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -26,19 +26,38 @@ def merge_sort( arr ):
     return merge(merge_sort(p1), merge_sort(p2))
   return arr
 
-print(merge_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7]))
+# print(merge_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7]))
 
 # STRETCH: implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
   # TO-DO
-
-  return arr
+  if len(arr) > 1:
+    L = 0
+    R = 0
+    left = arr[start:mid]
+    right = arr[mid:end]
+    print('merge_in_place', start, mid, end)
+    print('merge_in_place', left, right)
+    for i in range(start, end):
+      if R >= len(right) or (L < len(left) and left[L] < right[R]):
+        arr[i] = left[L]
+        L += 1
+      else:
+        arr[i] = right[R]
+        R += 1
+    print('merge_in_place', arr)
 
 def merge_sort_in_place(arr, l, r): 
-  # TO-DO
-
+  if r - l > 1:
+    mid = (l + r) // 2
+    merge_sort_in_place(arr, l, mid)
+    merge_sort_in_place(arr, mid, r)
+    merge_in_place(arr, l, mid, r)
   return arr
 
+newArr = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+merge_sort_in_place(newArr, 0, len(newArr))
+print(newArr)
 
 # STRETCH: implement the Timsort function below
 # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
